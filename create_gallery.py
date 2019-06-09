09af0b6955f88e521a62410810ec338fef85fc6b
Jamison Lahman <lahmanja@protonmail.com>
2019-06-08 21:54:56 -0700

    fill gallery

---
+++ create_gallery.py
@@ -0,0 +1,13 @@
+from os import listdir
+
+from os.path import isfile, join
+mypath  = "./images/gallery"
+onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+i = len(onlyfiles)
+for file in onlyfiles:
+    html =    '        <div id="' + str(i) + '" class="frame">\n' \
+            + '          <img src="/images/gallery' + file + '" alt="TODO(jamison) Add alt"/>\n' \
+            + '          <p class="caption">TODO(jamison) Add caption.</p>\n' \
+            + '        </div>'
+    i -= 1
+    print (html)
