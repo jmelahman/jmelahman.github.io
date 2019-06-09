37258b0c06f8333d521ae8c41f05e44086755e65
Jamison Lahman <lahmanja@protonmail.com>
2019-06-09 03:13:15 -0700

    Update create_gallery.py

--- create_gallery.py
+++ create_gallery.py
@@ -19,7 +19,7 @@ sorted_files = sorted(files_dict.items(), key=operator.itemgetter(1), reverse=Tr
 i = len(sorted_files)
 for key, file in enumerate(sorted_files):
     html = '        <div id="' + str(i) + '" class="frame">\n' \
-         + '          <a href="/gallery/#' + str(i) + '">\n' \
+         + '          <a href="/gallery#' + str(i) + '">\n' \
          + '            <div class="frame-border">\n' \
          + '              <img src="/images/gallery/' + file[0] + '" alt="TODO(jamison) Add alt"/>\n' \
          + '              <p class="caption">TODO(jamison) Add caption.</p>\n' \
