b0220c74585242e5c1194cb17d17d3602c3da275
Jamison Lahman <lahmanja@protonmail.com>
2019-06-08 22:01:59 -0700

    add border to images

--- create_gallery.py
+++ create_gallery.py
@@ -6,6 +6,7 @@ onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
 i = len(onlyfiles)
 for file in onlyfiles:
     html =    '        <div id="' + str(i) + '" class="frame">\n' \
+            + '          <div class="frame-border">"\n' \
             + '          <img src="/images/gallery/' + file + '" alt="TODO(jamison) Add alt"/>\n' \
             + '          <p class="caption">TODO(jamison) Add caption.</p>\n' \
             + '        </div>'
