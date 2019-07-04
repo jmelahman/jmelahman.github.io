6a805d16472fc99c2e738f84342d7ca86dd61ed4
Jamison Lahman <lahmanja@protonmail.com>
2019-07-03 19:37:08 -0700

    full site works offline, fixed javascript

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,7 @@
 /* When the user clicks on the button,
 toggle between hiding and displaying the menu */
 function pushMenu(id) {
-  element = document.getElementById(id)
+  element = document.getElementById(id);
   if (element.style.display !== "block") {
     element.style.display = "block";
   } else {
