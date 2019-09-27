56f996f06af29fd832dafd99b17a896fde0b2d02
Jamison Lahman <lahmanja@protonmail.com>
2019-09-26 23:01:56 -0700

    proper import

--- tools/base_needle_test.py
+++ tools/base_needle_test.py
@@ -1,20 +1,11 @@
 from needle.cases import NeedleTestCase
 from needle.driver import NeedleFirefox
-
-def import_from_string(path):
-    """
-    Utility function to dynamically load a class specified by a string,
-    e.g. 'path.to.my.Class'.
-    """
-    module_name, klass = path.rsplit('.', 1)
-    module = __import__(module_name, fromlist=[klass])
-    return getattr(module, klass)
+from needle.engines.pil_engine import Engine
 
 class BaseNeedleTest(NeedleTestCase):
 
     def setUp(self):
-        klass = import_from_string('needle.engines.pil_engine.Engine')
-        self.engine = klass()
+        self.engine = Engine()
         self.driver = NeedleFirefox()
         self._base_url = "https://jmelahman.github.io/index.html"
 
